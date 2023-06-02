02a3e2e8f7aa05cfe6252122e8b0c679b445649f
Евгений Ивашин <alladux@yandex.ru>
2023-06-02 10:54:49 +0300

    user API

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,10 +1,17 @@
 from django.contrib.auth import views
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .views import UserViewSet
 
 app_name = "users"
 
+router = routers.DefaultRouter()
+router.register(r"users", UserViewSet, basename="users")
+
 urlpatterns = [
     path("auth/token/", TokenObtainPairView.as_view(), name="get_token"),
-    path("auth/signup/", views.PasswordResetView.as_view()),
+    # path("auth/signup/", views.PasswordResetView.as_view()),
+    path("", include(router.urls)),
 ]
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,6 +1,13 @@
 import secrets
 import string
 from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions, filters, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from .models import CustomUser
+from .serializers import UserSerializer
 
 
 def generate_code():
@@ -21,3 +28,37 @@ def new_user(email, confirmation_code):
         from_email=None,
         recipient_list=[email],
     )
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAdminUser,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("username",)
+
+    @action(
+        detail=False,
+        methods=["get", "patch"],
+        permission_classes=[permissions.IsAuthenticated],
+        url_path="me",
+    )
+    def me_about(self, request):
+        user = get_object_or_404(CustomUser, username=request.user.username)
+        if request.method == "PATCH":
+            serializer = UserSerializer(user, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_200_OK,
+                )
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        serializer = self.get_serializer(user)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+        )
