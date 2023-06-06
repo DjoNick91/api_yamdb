1e3968a2479bb9f16060c5838509186c50ce013a
Евгений Ивашин <alladux@yandex.ru>
2023-06-06 17:49:13 +0300

    fix admin and rewiev models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -4,24 +4,24 @@ from .models import Title, Category, Genre
 
 
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'genre', 'description')
-    search_fields = ('description', 'name')
-    list_filter = ('category', 'year')
-    empty_value_display = '-пусто-'
+    list_display = ("name", "category", "description")
+    search_fields = ("description", "name")
+    list_filter = ("category", "year")
+    empty_value_display = "-пусто-"
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    search_fields = ('name', 'slug')
-    list_filter = ('name',)
-    empty_value_display = '-пусто-'
+    list_display = ("name", "slug")
+    search_fields = ("name", "slug")
+    list_filter = ("name",)
+    empty_value_display = "-пусто-"
 
 
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    search_fields = ('name', 'slug')
-    list_filter = ('name',)
-    empty_value_display = '-пусто-'
+    list_display = ("name", "slug")
+    search_fields = ("name", "slug")
+    list_filter = ("name",)
+    empty_value_display = "-пусто-"
 
 
 admin.site.register(Title, TitleAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,19 +3,15 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(
-        'Название',
+        "Название",
         max_length=256,
     )
-    slug = models.SlugField(
-        'Слаг',
-        max_length=50,
-        unique=True
-    )
+    slug = models.SlugField("Слаг", max_length=50, unique=True)
 
     class Meta:
-        ordering = ('name',)
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        ordering = ("name",)
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
@@ -23,19 +19,15 @@ class Category(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(
-        'Название',
+        "Название",
         max_length=256,
     )
-    slug = models.SlugField(
-        'Слаг',
-        max_length=50,
-        unique=True
-    )
+    slug = models.SlugField("Слаг", max_length=50, unique=True)
 
     class Meta:
-        ordering = ('name',)
-        verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'
+        ordering = ("name",)
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
 
     def __str__(self):
         return self.slug
@@ -43,38 +35,36 @@ class Genre(models.Model):
 
 class Title(models.Model):
     category = models.ForeignKey(
-        Category,
-        verbose_name='Slug категории',
+        Category, verbose_name="Slug категории", on_delete=models.CASCADE
     )
-    genre = models.ManyToManyField(
-        Genre,
-        through='TitleGenre'
-    )
+    genre = models.ManyToManyField(Genre, through="GenreTitle")
     name = models.CharField(
-        'Название',
+        "Название",
         max_length=256,
     )
-    year = models.IntegerField('Год выпуска')
+    year = models.IntegerField("Год выпуска")
 
-    description = models.TextField(
-        'Описание',
-        blank=True,
-        null=True
-    )
+    description = models.TextField("Описание", blank=True, null=True)
 
     class Meta:
-        ordering = ('year', 'name')
-        verbose_name = 'Произведение'
-        verbose_name_plural = 'Произведения'
-        default_related_name = 'titles'
+        ordering = ("year", "name")
+        verbose_name = "Произведение"
+        verbose_name_plural = "Произведения"
+        default_related_name = "titles"
 
     def __str__(self):
         return self.name
 
 
 class GenreTitle(models.Model):
-    genre_id = models.ForeignKey(Genre)
-    title_id = models.ForeignKey(Title)
+    genre_id = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+    )
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
-        return f'{self.genre} {self.title}'
+        return f"{self.genre} {self.title}"
