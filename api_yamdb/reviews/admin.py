aded8839c7ed048e044564f170c55a9075eb84be
Евгений Ивашин <alladux@yandex.ru>
2023-06-09 14:07:54 +0300

    fix admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -19,14 +19,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "slug")
     search_fields = ("name", "slug")
     list_filter = ("name",)
-    empty_value_display = "-пусто-"
+    empty_value_display = "пусто"
 
 
 class GenreAdmin(admin.ModelAdmin):
     list_display = ("name", "slug")
     search_fields = ("name", "slug")
     list_filter = ("name",)
-    empty_value_display = "-пусто-"
+    empty_value_display = "пусто"
 
 
 admin.site.register(Title, TitleAdmin)
