24927c0c469abf463fcfd417d8ad173d9b29c781
ilik1799 <264407@edu.itmo.ru>
2023-06-09 14:36:53 +0300

    Фикс символа в названии класса

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from .models import CustomUser
 
 
-admin.site.register(CustomUser, admin.ModelAdmin)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
